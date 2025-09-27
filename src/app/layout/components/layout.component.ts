89161b61bc1906b765050e1e79a587eac6ab8c09
HeiderHDev <heiderhrnndz@gmail.com>
2025-09-27 17:52:20 -0500

    feat(layout): implement layout component with responsive toolbar and sidenav

---
+++ src/app/layout/components/layout.component.ts
@@ -0,0 +1,53 @@
+import { MediaMatcher } from '@angular/cdk/layout';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { RouterModule } from '@angular/router';
+import { MenuItem } from '../interface/menu-item.interface';
+
+const materialModules = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule,
+  MatTooltipModule,
+  MatSidenavModule,
+  MatListModule,
+];
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [RouterModule, CommonModule, ...materialModules],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss',
+})
+export class LayoutComponent {
+  public mobileQuery!: MediaQueryList;
+  public menuItems: MenuItem[] = [];
+  private _mobileQueryListener: () => void;
+
+  constructor() {
+    const changeDetectorRef = inject(ChangeDetectorRef);
+    const media = inject(MediaMatcher);
+
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addEventListener('change', this._mobileQueryListener);
+  }
+
+  public ngOnInit(): void {
+    this.menuItems = [
+      { link: 'prueba1', label: 'prueba1', icon: 'task' },
+      { link: 'prueba2', label: 'prueba2', icon: 'sync' },
+    ];
+  }
+
+  public ngOnDestroy(): void {
+    this.mobileQuery.removeEventListener('change', this._mobileQueryListener);
+  }
+}
