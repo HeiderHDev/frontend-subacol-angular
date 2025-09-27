5a0328db6f86e665a72ceff395d59ad99f63ee37
HeiderHDev <heiderhrnndz@gmail.com>
2025-09-27 18:28:28 -0500

    feat(app): integrate loading spinner and toast notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { NgxToastModule } from '@angular-magic/ngx-toast';
+import {
+  ChangeDetectorRef,
+  Component,
+  DestroyRef,
+  inject,
+} from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { RouterOutlet } from '@angular/router';
+import { LoadingService } from '@core/services/loading.service';
+import { SpinnerComponent } from './core/components/spinner/spinner.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgxToastModule, SpinnerComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'frontend-subacol-angular';
+  public loading = false;
+  private readonly _loadingService = inject(LoadingService);
+  private readonly cdr = inject(ChangeDetectorRef);
+  private readonly destroyRef = inject(DestroyRef);
+
+  public ngOnInit(): void {
+    this.listenToLoading();
+  }
+
+  private listenToLoading(): void {
+    this._loadingService
+      .isLoading()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((loading) => {
+        this.loading = loading;
+        this.cdr.detectChanges();
+      });
+  }
 }
