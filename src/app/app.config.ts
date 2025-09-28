17bf3503de17c6b9d3a0e5a7f71622ebd0e95b87
HeiderHDev <heiderhrnndz@gmail.com>
2025-09-28 00:13:28 -0500

    refactor(config): reorganize imports in app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,12 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import {
   provideHttpClient,
   withFetch,
   withInterceptors,
 } from '@angular/common/http';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
 import { loadingInterceptor } from '@core/interceptors/loading.interceptor';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
