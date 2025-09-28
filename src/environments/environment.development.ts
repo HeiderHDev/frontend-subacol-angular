5d107000048e269db53e1cae71b6587f3947fd15
HeiderHDev <heiderhrnndz@gmail.com>
2025-09-27 19:24:11 -0500

    feat(env): add environment configuration for development and production

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  theMovieDB: {
+    apiUrl: 'https://api.themoviedb.org/3',
+    apiKey: '{{TMDB_API_KEY}}',
+    imageBaseUrl: 'https://image.tmdb.org/t/p/',
+    imageSizes: {
+      poster: 'w500',
+      backdrop: 'w1280',
+      profile: 'w185',
+    },
+  },
+};
