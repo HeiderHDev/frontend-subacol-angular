1f89db065ad88c8bb6894c71a96b6a91ca59c1ff
HeiderHDev <heiderhrnndz@gmail.com>
2025-09-29 01:26:11 -0500

    feat(routes): add routes for movie creation, editing, details, my-movies, and favorites

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -20,6 +20,41 @@ export const routes: Routes = [
             './features/movies/components/movie-list/movie-list.component'
           ).then((c) => c.MovieListComponent),
       },
+      {
+        path: 'movies/create',
+        loadComponent: () =>
+          import(
+            './features/movies/components/movie-form/movie-form.component'
+          ).then((c) => c.MovieFormComponent),
+      },
+      {
+        path: 'movies/edit/:id',
+        loadComponent: () =>
+          import(
+            './features/movies/components/movie-form/movie-form.component'
+          ).then((c) => c.MovieFormComponent),
+      },
+      {
+        path: 'movies/:id',
+        loadComponent: () =>
+          import(
+            './features/movies/pages/movie-details/movie-details.component'
+          ).then((c) => c.MovieDetailsComponent),
+      },
+      {
+        path: 'my-movies',
+        loadComponent: () =>
+          import(
+            './features/movies/components/my-movies/my-movies.component'
+          ).then((c) => c.MyMoviesComponent),
+      },
+      {
+        path: 'favorites',
+        loadComponent: () =>
+          import(
+            './features/movies/components/favorites/favorites.component'
+          ).then((c) => c.FavoritesComponent),
+      },
     ],
   },
   {
